fd8c167fa387f2bbbc354b0b78d0da74ffa8bb33
abdullah628 <abdullah628360@gmail.com>
2024-02-03 18:39:14 +0600

    check edit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,7 +26,7 @@ const Hero = () => {
         </p>
 
         <CustomButton
-          title="Explore Cars"
+          title="Explore Carss"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
